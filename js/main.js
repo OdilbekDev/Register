9ac587532bf2601c2eb246a0a130fa8f82d7a44d
DesmonProgrammer <desmon.programmer@gmail.com>
2022-08-11 13:48:43 +0500

    '3

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var first_name = prompt("Enter your firstname: ")
 var last_name = prompt("Enter your lastname:")
 var phone = prompt("Enter your contact number:")
 
+alert("Muvaffaqiyatli ro'yhatdan o'tdingiz")
+
 console.log(first_name);
 console.log(last_name);
 console.log(phone);
