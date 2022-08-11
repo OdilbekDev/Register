7192242a3e396b3164113e46cd4bdc7e231d940d
DesmonProgrammer <desmon.programmer@gmail.com>
2022-08-11 13:42:45 +0500

    '1'

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var first_name = prompt("Enter your firstname: ")
+var last_name = prompt("Enter your lastname:")
+var phone = prompt("Enter your contact number:")
+
+console.log(first_name);
+console.log(last_name);
+console.log(phone);
